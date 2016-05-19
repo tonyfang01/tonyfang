5ed3f86ef81a062bc9aa76e31d5e471d31e98bfc
Tony Fang <tonyfang801@gmail.com>
2016-05-19 23:04:54 +0000

    /

--- site.rb
+++ site.rb
@@ -2,9 +2,9 @@ require 'bundler/setup'
 require 'sinatra'
 set :port, 4000
 set :bind, '0.0.0.0'
-get '/' do
-  "I'm alive!"
-end
+ get '/' do
+   erb :index
+ end
 get '/italian' do
   "Ciao!"
 end
